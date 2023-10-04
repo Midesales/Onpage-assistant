65a3b9ae90c37f7798dfd7a36d96fcf2dc93b825
Ayomide <@vayomide2@gmail.com>
2023-10-04 14:33:27 +0100

    record

--- src/component/record.jsx
+++ src/component/record.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './header'
 import Footer from './footer'
 import Video from './video'
+import logo from '../images/iCON 7 1.png'
 
 function Record() {
   return (
